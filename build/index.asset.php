a3a58f6ee5279c46e16bd293535af181cc257b1e
osony <osony@softserveinc.com>
2023-03-15 13:31:59 +0200

    Save progress for package json file

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '09ec51d37ddcb9a74fb9');
---
+++ build/index.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const e=window.wp.element;(0,window.wp.blocks.registerBlockType)("core/firstblock",{edit:function(){return(0,e.createElement)("p",{className:"class"},'"Edit"')},save:function(){return(0,e.createElement)("p",{className:"class"},'"Save"')}})})();
